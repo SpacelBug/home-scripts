cdaa9544c8e0318d837712b616b46e65b5348b68
SpacelBug <commander.n1@mail.ru>
2023-06-27 14:01:45 +1200

    change default values and small fixes

--- sentries/dataset_process.py
+++ sentries/dataset_process.py
@@ -2,11 +2,12 @@ import os
 from .images_sentry import get_images_list
 
 
-def create_captions(path, default_caption=None):
+def create_captions(path, default_caption=''):
     for file in get_images_list(path):
         os.chdir(path)
-        with open(f"{path}/{file.name.split('.')[0]}.txt", 'a+') as caption_file:
-            try:
-                caption_file.write(default_caption)
-            except FileNotFoundError:
-                print(f'File not found: path="{path}/{file.name}"')
+        if file.get('name'):
+            with open(f"{path}/{file.get('name').split('.')[0]}.txt", 'w+') as caption_file:
+                try:
+                    caption_file.write(default_caption)
+                except FileNotFoundError:
+                    print(f'File not found: path="{path}/{file.name}"')
