c19a1f0e9670670ad00ff2c1fa04090919719604
I love kludge <98ger@mail.ru>
2024-01-12 14:46:03 +1200

    add function for navigate in directories

--- sentries/explorer.py
+++ sentries/explorer.py
@@ -19,7 +19,9 @@ class File:
 class Directory:
 
     def __init__(self, path: str):
-        self.path = path
+        self.path = os.path.abspath(path)
+        self.name = os.path.basename(self.path)
+        self.parent_path = os.path.dirname(self.path)
 
         files_params = []
         dirs_path = []
@@ -41,3 +43,26 @@ class Directory:
     def __directories_genetator(self, dirs_path: list):
         for path in dirs_path:
             yield Directory(path)
+
+    def navigate(self) -> None:
+
+        while True:
+
+            directories = [Directory(self.parent_path)] + [directory for directory in self.dirs]
+            print(f"\nDirectories in '{self.name}':")
+
+            counter = 0
+            for directory in directories:
+                print(f"    {counter}) '{directory.name}'")
+                counter += 1
+
+            try:
+                selected_directory_index = int(input('Select dir by number (symbol will stop navigation) '))
+            except ValueError as error:
+                break
+
+            if (selected_directory_index >= 0) and (selected_directory_index < len(directories)):
+                self.__init__(directories[selected_directory_index].path)
+            else:
+                print('Wrong index!')
+                self.__init__(self.path)
