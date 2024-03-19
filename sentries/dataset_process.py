34406c212db74810ff6396242135207a8bb4c4c9
I love kludge <98ger@mail.ru>
2024-03-20 11:11:23 +1200

    rewrite create_captions() in dataset_process.py with class

--- sentries/dataset_process.py
+++ sentries/dataset_process.py
@@ -10,10 +10,9 @@ def create_captions(path, default_caption=''):
     :param default_caption значение для текстовых файлов по умолчанию.
     """
     for file in get_images_list(path):
-        os.chdir(path)
-        if file.get('name'):
-            with open(f"{path}/{file.get('name').split('.')[0]}.txt", 'w+') as caption_file:
-                try:
-                    caption_file.write(default_caption)
-                except FileNotFoundError:
-                    print(f'File not found: path="{path}/{file.name}"')
+
+        with open(f"{path}/{file.name.split('.')[0]}.txt", 'w+') as caption_file:
+            try:
+                caption_file.write(default_caption)
+            except FileNotFoundError:
+                print(f'File not found: path="{path}/{file.name}"')
